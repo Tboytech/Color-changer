749f056ee98fc3762f460d27f315a95492d52234
olayinka babaunde <babatundeolayinka28@gmail.com>
2024-07-01 00:14:54 +0100

    color changer

---
+++ index.js
@@ -0,0 +1,13 @@
+document.addEventListener(`DOMContentLoaded`, ()=> {
+    const Box = document.getElementById(`Box`)
+    const change = document.getElementById(`Change`)
+
+    const randomColor = () => {
+        const letters = "0123456789ABCDEF"
+        return `#` + Array.from({length: 6}).map(() => letters[Math.floor(Math.random() * 16)
+        ]).join(``)
+    }
+    change.addEventListener(`click`, () => {
+        Box.style.backgroundColor = randomColor()
+    })
+})
